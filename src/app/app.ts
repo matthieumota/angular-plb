e206e83c0a165a97acb687fad8c85c5619070073
Matthieu Mota <matthieumota@gmail.com>
2026-07-15 13:38:14 +0200

    begin angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Pizz, Pizza } from './models/pizza';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('pizzaparty');
+  protected readonly title = 'pizzaparty';
+  pizza0: Pizz = {
+    id: 1,
+    name: '4 fromages',
+    price: 12,
+    image: '/assets/pizzas/reine.jpg',
+  }
+  pizza: Pizza = new Pizza(1, 'Reine', 12, '/assets/pizzas/reine.jpg');
 }
---
+++ src/app/models/pizza.ts
@@ -0,0 +1,20 @@
+export class Pizza {
+  id: number;
+  name: string;
+  price: number;
+  image?: string;
+
+  constructor(id: number, name: string, price: number, image?: string) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.image = image;
+  }
+}
+
+export type Pizz = {
+  id: number
+  name: string
+  price: number
+  image?: string
+}
