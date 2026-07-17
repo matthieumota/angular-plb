f305b6206c217a6e5a00e151c3c3e883d61bf560
Matthieu Mota <matthieumota@gmail.com>
2026-07-17 09:19:21 +0200

    clean app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { NgClass } from '@angular/common';
-import { Counter } from "./components/counter/counter";
 import { MessageService } from './services/message-service';
 import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule, NgClass, Counter, RouterOutlet, RouterLink],
+  imports: [FormsModule, RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -15,11 +13,4 @@ export class App {
   protected readonly title = 'pizzaparty';
 
   messageService = inject(MessageService);
-
-  addMessage(): void {
-    const types = ['success', 'error', 'info', 'warning'];
-    const type = types[Math.floor(Math.random() * types.length)];
-
-    this.messageService.addMessage({ text: 'test', type });
-  }
 }
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Counter } from "../../components/counter/counter";
+import { MessageService } from '../../services/message-service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Counter, NgClass],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  messageService = inject(MessageService);
+
+  addMessage(): void {
+    const types = ['success', 'error', 'info', 'warning'];
+    const type = types[Math.floor(Math.random() * types.length)];
+
+    this.messageService.addMessage({ text: 'test', type });
+  }
+}
